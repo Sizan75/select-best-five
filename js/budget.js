af820b5189fce24f590e980ad02b8c2a0948d071
Sizan75 <sakiuzzaman26@gmail.com>
2022-08-22 16:50:10 +0600

    Player Total Calculation Done

--- js/budget.js
+++ js/budget.js
@@ -16,3 +16,19 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
     const total = newPlayerCost * listLength ;
     playerTotalExpenses.innerText = total ;
 })
+
+document.getElementById('btn-calculate-total').addEventListener('click',function(){
+    const managerCost = getValueById('manager-expense');
+    const coachCost = getValueById('coach-expense');
+
+    const playerTotalExpenses= document.getElementById('player-total-expenses');
+    const playerTotalExpensesString = playerTotalExpenses.innerText;
+    const playerTotalCost = parseFloat(playerTotalExpensesString);
+
+    const total= document.getElementById('total-expenses');
+    const totalString = total.innerText;
+    const totalExpenses = parseFloat(totalString);
+
+    const newTotal = managerCost + coachCost + playerTotalCost ;
+    total.innerText=newTotal;
+})
